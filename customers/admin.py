98b672131b6a871fda7e6cb88d42c2bbca027a06
Cassio Da Silva De Jesus <cassios@g.globo>
2026-07-02 11:53:38 -0300

    Configurando admin do django

--- customers/admin.py
+++ customers/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from customers.models import Customer
 
-# Register your models here.
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'phone_number', 'address')
+    search_fields = ('name', 'email', 'phone_number')
+    list_filter = ('name', 'email')
+
--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from orders.models import Order
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'customer', 'status', 'total_amount', 'created_at')
+    search_fields = ('customer__name', 'status')
+    list_filter = ('status', 'created_at')   
